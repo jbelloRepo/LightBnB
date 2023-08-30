ad1f58bd9c2129615df5cbb6ce98ffe35277046f
Jubril Bello <jbello@uoguelph.ca>
2023-08-30 04:12:20 -0400

    Connect boilerplate to PostgreSQL

--- LightBnB/LightBnB_WebApp-master/db/database.js
+++ LightBnB/LightBnB_WebApp-master/db/database.js
@@ -1,6 +1,19 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+// the following assumes that you named your connection variable `pool`
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
+
+
+
 /// Users
 
 /**
